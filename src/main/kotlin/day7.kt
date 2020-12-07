b3d18ae96b9320f76b8120676ac422a1dae00c9d
Dimitris Kritikos <just_jim@live.com>
2020-12-08 00:10:27 +0100

    WIP day 7

--- src/main/kotlin/day7.kt
+++ src/main/kotlin/day7.kt
@@ -1,7 +1,29 @@
 fun main() {
 
-    val data = readFileAsLinesUsingUseLines("src/customs")
+    val data = readFileAsLinesUsingUseLines("src/jim")
+    val bags = mutableMapOf<String,Bag>()
     data.forEach { line ->
+        val parts = line.split("bags contains")
+        val bag = decodeBag(parts[0])
+
+        bags.putIfAbsent(bag.name,bag)
+        parts[0].split(",").forEach{
+
+        }
 
     }
+}
+
+fun decodeBag(string: String): Bag
+{
+    val parts = string.split(" ")
+    return Bag(parts[0]+parts[1])
+}
+
+class Bag(
+    var name : String,
+    var parents : MutableSet<Bag> = mutableSetOf()
+)
+{
+
 }
