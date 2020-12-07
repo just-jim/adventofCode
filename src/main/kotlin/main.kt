d81b6c8f3093b629df98436b4ec42ffc2809e570
just_jim <dimitris@pleo.io>
2020-12-07 14:57:19 +0100

    day 6

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -39,8 +39,8 @@ fun main(args: Array<String>) {
             valid2++
     }
 
-    println("Day 2 Part 1 solution: $valid1")
-    println("Day 2 Part 2 solution: $valid2")
+    println("Day 2 Part 1: $valid1")
+    println("Day 2 Part 2: $valid2")
     */
 
     /*// Day 3
@@ -65,7 +65,7 @@ fun main(args: Array<String>) {
                 trees++
         }
     }
-    println("Day 3 Part 1 solution: $trees")
+    println("Day 3 Part 1: $trees")
 
     // Part 2
     val stepList : Array<Pair<Int,Int>> = Array(5) {Pair(1,1)}
@@ -90,7 +90,7 @@ fun main(args: Array<String>) {
         sol *= trees
     }
 
-    println("Day 3 Part 1 $sol")
+    println("Day 3 Part 1: $sol")
     */
 
     /*// Day 4
@@ -110,23 +110,26 @@ fun main(args: Array<String>) {
             }
             curPassport = Passport()
         }
-        line.split(" ").forEach {
-            val info = it.split(":")
-            when(info[0].toLowerCase()){
-                "byr" -> curPassport?.byr = info[1]
-                "iyr" -> curPassport?.iyr = info[1]
-                "eyr" -> curPassport?.eyr = info[1]
-                "hgt" -> curPassport?.hgt = info[1]
-                "hcl" -> curPassport?.hcl = info[1]
-                "ecl" -> curPassport?.ecl = info[1]
-                "pid" -> curPassport?.pid = info[1]
-                "cid" -> curPassport?.cid = info[1]
+        else
+        {
+            line.split(" ").forEach {
+                val info = it.split(":")
+                when(info[0].toLowerCase()){
+                    "byr" -> curPassport?.byr = info[1]
+                    "iyr" -> curPassport?.iyr = info[1]
+                    "eyr" -> curPassport?.eyr = info[1]
+                    "hgt" -> curPassport?.hgt = info[1]
+                    "hcl" -> curPassport?.hcl = info[1]
+                    "ecl" -> curPassport?.ecl = info[1]
+                    "pid" -> curPassport?.pid = info[1]
+                    "cid" -> curPassport?.cid = info[1]
 
+                }
             }
         }
     }
-    println("Day 4 Part 1 ${passports.size}")
-    println("Day 4 Part 2 ${passports2.size}")
+    println("Day 4 Part 1: ${passports.size}")
+    println("Day 4 Part 2: ${passports2.size}")
     */
 
     /*// Day 5
@@ -160,49 +163,88 @@ fun main(args: Array<String>) {
     val index = seats.joinToString(separator = "").indexOf("101")+1
     val mySeat = index+(index%8)
 
-    println("Day 5 Part 1 $max")
-    println("Day 6 Part 2 $mySeat")
+    println("Day 5 Part 1: $max")
+    println("Day 6 Part 2: $mySeat")
     */
 
-    /*// Day 6 */
+    /*// Day 6
+    val data = readFileAsLinesUsingUseLines("src/customs")
+    var group: MutableSet<Char> = mutableSetOf()
+    var group2: MutableMap<Char,Int> = mutableMapOf()
+    var count = 0
+    var count2 = 0
+    var groupMembers = 0
+    data.forEach { line ->
+        if(line == ""){
+            count += group.size
+            group = mutableSetOf()
+
+            group2.forEach{
+                if (it.value == groupMembers)
+                    count2++
+            }
+            group2 = mutableMapOf()
+            groupMembers = 0
+        }
+        else
+        {
+            groupMembers++
+            line.toCharArray().forEach {
+                group.add(it)
+                group2.putIfAbsent(it,0)
+                group2.merge(it,1, Int::plus)
+            }
+        }
+
+    }
+
+    println("Day 5 Part 1: $count")
+    println("Day 5 Part 2: $count2")
+    */
+
+    /*// Day 7 */
+    val data = readFileAsLinesUsingUseLines("src/customs")
+    data.forEach { line ->
+
+    }
 }
 
 class Passport{
-    var byr : String? = null
-    var iyr : String? = null
-    var eyr : String? = null
-    var hgt : String? = null
-    var hcl : String? = null
-    var ecl : String? = null
-    var pid : String? = null
-    var cid : String? = null
+var byr : String? = null
+var iyr : String? = null
+var eyr : String? = null
+var hgt : String? = null
+var hcl : String? = null
+var ecl : String? = null
+var pid : String? = null
+var cid : String? = null
 
-    fun isValid(): Boolean {
-        return listOfNotNull(byr, iyr, eyr, hgt, hcl, ecl, pid).size == 7
-    }
+fun isValid(): Boolean {
+    return listOfNotNull(byr, iyr, eyr, hgt, hcl, ecl, pid).size == 7
+}
 
-    fun isValid2(): Boolean {
-        val v1 = byr?.toIntOrNull() in 1920..2002
-        val v2 = iyr?.toIntOrNull() in 2010..2020
-        val v3 = eyr?.toIntOrNull() in 2020..2030
-        val v4 = hgt?.matches("\\d+\\s*(?:cm|in)".toRegex())
-        val v5 = hcl?.matches("^#([a-fA-F0-9]{6})\$".toRegex())
-        val v6 = ecl?.matches("\\b(?:amb|blu|brn|gry|grn|hzl|oth)\\b".toRegex())
-        val v7 = pid?.length == 9 && pid?.matches("^\\d{9}\$".toRegex()) == true
-        var v8:Boolean = false
-        if(hgt?.takeLast(2) == "cm")
-            v8 = hgt?.dropLast(2)?.toIntOrNull() in 150..193
-        else if(hgt?.takeLast(2) == "in")
-            v8 =hgt?.dropLast(2)?.toIntOrNull() in 59..76
+fun isValid2(): Boolean {
+    val v1 = byr?.toIntOrNull() in 1920..2002
+    val v2 = iyr?.toIntOrNull() in 2010..2020
+    val v3 = eyr?.toIntOrNull() in 2020..2030
+    val v4 = hgt?.matches("\\d+\\s*(?:cm|in)".toRegex())
+    val v5 = hcl?.matches("^#([a-fA-F0-9]{6})\$".toRegex())
+    val v6 = ecl?.matches("\\b(?:amb|blu|brn|gry|grn|hzl|oth)\\b".toRegex())
+    val v7 = pid?.length == 9 && pid?.matches("^\\d{9}\$".toRegex()) == true
+    var v8:Boolean = false
+    if(hgt?.takeLast(2) == "cm")
+        v8 = hgt?.dropLast(2)?.toIntOrNull() in 150..193
+    else if(hgt?.takeLast(2) == "in")
+        v8 =hgt?.dropLast(2)?.toIntOrNull() in 59..76
 
-        return isValid()&&v1&&v2&&v3&& v4 == true && v5 == true && v6 == true && v7 && v8
-    }
+    return isValid()&&v1&&v2&&v3&& v4 == true && v5 == true && v6 == true && v7 && v8
+}
 
-    override fun toString() = isValid2().toString()+": byr=$byr iyr=$iyr eyr=$eyr hgt=$hgt hcl=$hcl ecl=$ecl pid=$pid cid=$cid"
+override fun toString() = isValid2().toString()+": byr=$byr iyr=$iyr eyr=$eyr hgt=$hgt hcl=$hcl ecl=$ecl pid=$pid cid=$cid"
 }
 
 fun readFileAsLinesUsingUseLines(fileName: String): List<String>
 = File(fileName).useLines { it.toList() }
 
 fun array2dOfInt(sizeOuter: Int, sizeInner: Int): Array<IntArray>
-    = Array(sizeOuter) { IntArray(sizeInner) }
+= Array(sizeOuter) { IntArray(sizeInner) }
