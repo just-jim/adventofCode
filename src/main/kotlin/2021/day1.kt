ce308f59890704c99597fc121e67676fb2a591f4
Dimitris Kritikos <just_jim@live.com>
2021-12-07 23:10:46 +0100

    2021 day1

---
+++ src/main/kotlin/2021/day1.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val test = false
+    val file = readFileAsInt(if(test) "jim" else "2021/day1")
+    var count = 0
+    file.forEachIndexed{i,v ->
+        if(i==0) return@forEachIndexed
+        if(v>file[i-1]) count++
+    }
+    println("Part 1: $count")
+
+    count = 0
+    file.forEachIndexed{i,v ->
+        if(i>=file.size-3) return@forEachIndexed
+        if(v+file[i+1]+file[i+2]<file[i+1]+file[i+2]+file[i+3]) count++
+    }
+    println("Part 2: $count")
+}
