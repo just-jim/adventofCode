cff276daa2e16d97854772d15caeb098d2ecb65f
Just_Jim <dimitris@pleo.io>
2022-12-04 02:27:17 +0000

    Improvements
    - Add  kotlin-algorithm-club library
    - Bump kotlin version to 1.7
    - cleanup build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.7.21"
+    id("org.jmailen.kotlinter") version "3.11.0"
     application
 }
 
@@ -14,21 +15,5 @@ repositories {
 
 dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClassName = "MainKt"
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    testImplementation("junit:junit:4.13.1")
 }
