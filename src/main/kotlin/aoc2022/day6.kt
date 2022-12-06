180514753353ca5782dabdaef9d6556519a03a56
Just_Jim <dimitris@pleo.io>
2022-12-06 09:46:24 +0100

    2022 day 6

---
+++ src/main/kotlin/aoc2022/day6.kt
@@ -0,0 +1,25 @@
+package aoc2022
+
+import readFileAsStrings
+
+fun main() {
+    val test = false
+    val file = readFileAsStrings(if (test) "sample" else "aoc2022/day6")
+    val signal = file.first()
+
+    fun String.detectMarker(bufferLength: Int): Int? {
+        (bufferLength until this.length).forEach {
+            val buffer = this.subSequence(it - bufferLength, it).toList()
+            if (buffer.distinct().count() == buffer.count()) {
+                return it
+            }
+        }
+        return null
+    }
+
+    val sol1 = signal.detectMarker(4)
+    println("Part 1: $sol1")
+
+    val sol2 = signal.detectMarker(14)
+    println("Part 2: $sol2")
+}
